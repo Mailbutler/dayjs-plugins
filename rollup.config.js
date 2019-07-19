ba1994b18d1e1b80d7e40f7f3d33f4d4fc19326b
Stanyslas Bres <stanyslas.bres@gmail.com>
2019-07-19 11:42:03 +0200

    fix(rollup): set static bundle name

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,12 @@
 const babel = require('rollup-plugin-babel');
 const uglify = require('rollup-plugin-uglify');
 
-const name = require('./package.json').name;
-
 module.exports = {
 	input: './src/index.js',
 	external: ['dayjs'],
 	plugins: [babel({ exclude: 'node_modules/**' }), uglify()],
 	output: {
-		file: `./dist/${name}.min.js`,
+		file: `./dist/dayjs-plugins.min.js`,
 		format: 'umd',
 		name,
 		globals: { dayjs: 'dayjs' }
